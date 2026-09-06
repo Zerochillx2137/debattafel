9fc5674f52f287287e9ebacf86f1c4197ba97434
Zerochillx2137 <slowik.kry@gmail.com>
2026-09-06 20:17:34 +0200

    Delete debattafel directory

--- debattafel/tests/services.test.js
+++
@@ -1,4 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import {categories} from '../src/services/news-service.js';
-test('ondersteunt alle negen nieuwsfilters',()=>assert.equal(categories.length,9));
